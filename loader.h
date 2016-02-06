60332cc81aa59ceee427a0e12e384ff8f5c0c378
SonyUSA <sirius.cybernetics@gmail.com>
2016-02-06 16:26:30 -0700

    Update loader.h

--- loader.h
+++ loader.h
@@ -13,16 +13,24 @@ struct cGlobals {
 	int row;
 	int col;
 	int level;
+	int dogsteps;
+	int dogalive;
+	int mysteps;
+	int maxhealth;
+	int curhealth;
 	// Unlockables
 	// Some Buffers
 	char info1[256];
 	char titlebar1[128];
 	char titlebar2[128];
+	char titlebar3[128];
 	char stats[256];
 	char map[256];
 	char endgame[256];
 	char player1[8];
 	char doggy[8];
+	char dogstat[64];
+	char mystat[64];
 	char mapbuff[2800];
 	// Array
 	unsigned char nMapArray[17][62];
